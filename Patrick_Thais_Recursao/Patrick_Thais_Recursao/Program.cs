d832001f8c4e99f26559d3db506fa662626c1baa
Patrick Scoralick Costa <patrickscoralick4@gmail.com>
2016-08-11 14:36:24 -0300

    Add files via upload

---
+++ Patrick_Thais_Recursao/Patrick_Thais_Recursao/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Patrick_Thais_Recursao
+{
+    class Program
+    {
+        static long Fatorial(int n)
+        {
+            
+            if (n <= 0)
+                return 1;
+            else return n * Fatorial(n - 1);
+        }
+
+    
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <= 6; i++)
+            {
+                
+                Console.WriteLine("{0} ! = {1}", i, Fatorial(i));
+            }
+            
+            Console.ReadLine();
+        }
+      }
+}
